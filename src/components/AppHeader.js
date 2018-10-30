aeea43569d72df244c407e552200d2007df6053c
Antony Padayattil <antonywired@gmail.com>
2018-10-30 11:11:20 +0000

    Add sidebar and header

---
+++ src/components/AppHeader.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import logo from '../images/logo.png';
+
+class AppHeader extends Component {
+  render() {
+    return (
+      <nav className="AppHeader navbar fixed-top navbar-expand-md navbar-light bg-white shadow">
+        <button class="navbar-toggler sidebar-toggler" type="button" onClick={() => this.props.toggleSidebar()}>
+          <span class="material-icons text-secondary">menu</span>
+        </button>
+        <a className="navbar-brand collapse navbar-collapse" href="#">
+          <img className="app-header-logo" src={logo} alt="" />
+        </a>
+        <button className="app-header-login collapse navbar-collapse btn btn-outline-success my-2 my-sm-0" type="submit">Log In</button>
+      </nav>
+    );
+  }
+}
+export default AppHeader;
