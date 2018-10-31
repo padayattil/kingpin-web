2f8f8186f22082879fc6d58b640c7f7b5dbf6faf
Antony Padayattil <antonywired@gmail.com>
2018-10-31 12:51:24 +0000

    sidebar

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -6,10 +6,10 @@ class AppHeader extends Component {
   render() {
     return (
       <nav className="AppHeader navbar fixed-top navbar-expand-md navbar-light bg-white shadow">
-        <button class="navbar-toggler sidebar-toggler" type="button" onClick={() => this.props.toggleSidebar()}>
-          <span class="material-icons text-secondary">menu</span>
+        <button className="navbar-toggler sidebar-toggler" type="button" onClick={() => this.props.toggleSidebar()}>
+          <span className="material-icons text-secondary">menu</span>
         </button>
-        <a className="navbar-brand collapse navbar-collapse" href="#">
+        <a className="navbar-brand" href="#">
           <img className="app-header-logo" src={logo} alt="" />
         </a>
         <button className="app-header-login collapse navbar-collapse btn btn-outline-success my-2 my-sm-0" type="submit">Log In</button>
